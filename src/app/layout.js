216fb4e80dcefeac17a31d80ed40f19e39882c03
Fikret Erkara <fikreterkara_26@hotmail.com>
2025-02-07 15:24:22 +0300

    created navbar component  and TopPages route

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Providers from "@/components/ThemeCom";
 import { ClerkProvider } from "@clerk/nextjs";
+import Navbar from "@/components/Navbar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -30,6 +31,7 @@ export default function RootLayout({ children }) {
         >
           <Providers>
             <Header />
+            <Navbar />
             {children}
           </Providers>
         </body>
---
+++ src/app/top/[genre]/page.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const TopPages = () => {
+  return (
+    <div>TopPages</div>
+  )
+}
+
+export default TopPages
