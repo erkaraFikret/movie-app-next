99187befbc831c22862646a9fcfc8cbbacbf26ee
Fikret Erkara <fikreterkara_26@hotmail.com>
2025-02-07 16:09:43 +0300

    added search movie functionality

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,6 +4,7 @@ import Header from "@/components/Header";
 import Providers from "@/components/ThemeCom";
 import { ClerkProvider } from "@clerk/nextjs";
 import Navbar from "@/components/Navbar";
+import SearchBox from "@/components/SearchBox";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,6 +33,7 @@ export default function RootLayout({ children }) {
           <Providers>
             <Header />
             <Navbar />
+            <SearchBox />
             {children}
           </Providers>
         </body>
